2078dd842192037d4268bce18b2c2c5ffe9653a8
diogoguilherme-dev <diogodg0707@gmail.com>
2026-02-12 18:20:05 -0300

    calculadora começando a funcionar, porém ainda com probelmas.

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-const botao = document.querySelector("#button-container")
+const convertButton = document.querySelector("#button-container")
+const valor = document.querySelector("#input-container")
+const valueToConvert = document.querySelector("#p6")
+const convertedValue = document.querySelector("#p8")
+
+
+function convertValeus() {
+    
+
+const valorN = Number(valor.value)
+const result = valorN * 5.2
+
+    valueToConvert.innerText = valor.value
+
+    convertedValue.innerText = result
+
+}
+
+convertButton.addEventListener("click", convertValeus)
 
