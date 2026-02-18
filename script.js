98179b10bf108b3f74339306be1a6c6795bacb42
diogoguilherme-dev <diogodg0707@gmail.com>
2026-02-18 19:57:06 -0300

    versão 2.0 (com as Libras)

--- script.js
+++ script.js
@@ -2,25 +2,67 @@ const convertButton = document.querySelector("#button-container")
 const valor = document.querySelector("#input-container")
 const valueToConvert = document.querySelector("#p6")
 const convertedValue = document.querySelector("#p8")
-
+const currencySelect = document.querySelector("#curency-select")
+const imgConvertedCurrency = document.querySelector("#img-converted-currency")
+const currencyName = document.querySelector("#currency-name")
 
 function convertValeus() {
+    const valorN = Number(valor.value) //valor em real
+    const dolarToday = 5.21 //valor do dolar
+    const euroToday = 6.18 //valor do euro
+    const libraToday = 7.07 //valor da libra
 
+    if (currencySelect.value == "dolar") {
 
-    const valorN = Number(valor.value)
-    const result = valorN * 5.21
+        imgConvertedCurrency.src = "./assets/estados-unidos-1.png"
+        imgConvertedCurrency.alt = "currency-dolar"
 
-    const valor1 = valorN.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL' });
+        currencyName.innerText = "Dólar"
 
-    const valor2 = result.toLocaleString('en-US', {style: 'currency', currency: 'USD' });
-    console.log(valor1)
-    console.log(valor2)
+        convertedValue.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(valorN / dolarToday)
 
-    valueToConvert.innerText = valor1
+    }
 
-    convertedValue.innerText = valor2
+    if (currencySelect.value == "euro") {
+
+        imgConvertedCurrency.src = "./assets/euro.png"
+        imgConvertedCurrency.alt = "currency-euro"
+
+        currencyName.innerText = "Euro"
+
+        convertedValue.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(valorN / euroToday)
+
+    }
+
+    if (currencySelect.value == "libra") {
+
+    imgConvertedCurrency.src = "./assets/libra-1.png"
+    imgConvertedCurrency.alt = "currency-libra"
+
+    currencyName.innerText = "Libra"
+
+    convertedValue.innerHTML = new Intl.NumberFormat("en-GB", {
+        style: "currency",
+        currency: "GBP"
+    }).format(valorN / libraToday)
+
+    }
+
+    valueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(valorN)
+    
+
 
 }
 
 convertButton.addEventListener("click", convertValeus)
+
 
